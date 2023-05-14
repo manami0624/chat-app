26296d9a26c423e102c766347cf3d0e6cee1844a
manami0624 <manana624@yahoo.co.jp>
2023-05-14 21:34:55 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
-root to: "messages#index"  #ビューを実装するための仮のルーティング設定
+  root to: "messages#index"  #ビューを実装するための仮のルーティング設定
+  resources :users, only: [:edit, :update]
 end
